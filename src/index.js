3e2890a35ffa0043d47321ffe3466966e47f75b4
Cory Dela Cruz <corydcruz@gmail.com>
2022-09-05 13:11:08 -0700

    changed bootswatch import to be in index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.css"; 
+import 'bootswatch/dist/lux/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.css"; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
